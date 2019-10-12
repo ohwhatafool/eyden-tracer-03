5c048c05acb84a5a4a44c09b61d4e9c8ec231b6c
Drish10 <drishti.serenity@gmail.com>
2019-10-12 23:58:38 +0200

    set up

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "Scene.h"
+#include "Scene.cpp"
 
 #include "CameraPerspective.h"
 
@@ -18,7 +18,7 @@ Mat RenderFrame(void)
 {
 	// Define a scene
 	CScene scene;
-	
+
 	// Load scene description
 	scene.ParseOBJ("../../../data/cow.obj");
 
@@ -26,28 +26,29 @@ Mat RenderFrame(void)
 	// Build BSPTree
 	scene.BuildAccelStructure();
 #endif
-	
+
 	Vec3f pointLightIntensity(3, 3, 3);
 	Vec3f lightPosition2(-3, 5, 4);
 	Vec3f lightPosition3(0, 1, 4);
-	
+
 	scene.Add(std::make_shared<CLightPoint>(pointLightIntensity, lightPosition2));
 	scene.Add(std::make_shared<CLightPoint>(pointLightIntensity, lightPosition3));
 
-	Mat img(scene.m_pCamera->getResolution(), CV_32FC3);		// image array
-	Ray ray;                                          			// primary ray
+	Mat img(scene.m_pCamera->getResolution(), CV_32FC3); // image array
+	Ray ray;											 // primary ray
 
 	for (int y = 0; y < img.rows; y++)
-		for (int x = 0; x < img.cols; x++) {
+		for (int x = 0; x < img.cols; x++)
+		{
 			scene.m_pCamera->InitRay(x, y, ray); // initialize ray
-			img.at<Vec3f>(y, x) = scene.RayTrace(ray); 
+			img.at<Vec3f>(y, x) = scene.RayTrace(ray);
 		}
-	
+
 	img.convertTo(img, CV_8UC3, 255);
 	return img;
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	DirectGraphicalModels::Timer::start("Rebdering frame... ");
 	Mat img = RenderFrame();
